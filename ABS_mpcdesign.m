c7618da18a66419250aefef26dc8ba82a2c52d32
pumpkink <pumpkinkhj@gmail.com>
2016-05-23 16:13:11 +0200

    MIMO

--- ABS_mpcdesign.m
+++ ABS_mpcdesign.m
@@ -10,6 +10,14 @@
 %% Fahrzeugparameter f¨¹r MPC Controller Design
 % Physical parameters
 g = 9.81;
+Pi = 3.1416;
+
+% E-Motor parameters
+T_sollrb = 100; % Desired Brake Moment von Rekuperation(max. T_rb ist 225Nm)
+k_motor = -51; % Slope of the n-M Kennlinie, bei max. Torque 15,1A/mm^2
+iG = 10; % Getriebe¨¹bersetzung zwischen Rad und Motor
+k_reifen = k_motor*iG/(2*Pi); % Kennziffer von w-T_rb Linear
+SOC  = 1;   % State of Charge (AKKU)
 
 % Fahrzeug parameters
 Td = 1500; % Desired Brake Moment 1500Nm
@@ -23,13 +31,25 @@ K_mu = 5.8160; % slope of s-u kurve
 m0 = m; % Initial mass (used in the EKF mass estimation)
 Fz = 4410; % Vertical Kraft Fz = m*g N
 %% Input/State/Output continuous time form(LTI)
-%  State Space Model
-A = [-a/v  1/v;
-     (a*R^2*Fz*K_mu)/(J*v)   -(Fz*K_mu*R^2)/(J*v)];
-B = [0;
-    R/J];
-C = [Fz*K_mu*R  J];
-D = [0];
+%  State Space Modell
+
+% A = nxn = 3x3
+A = [-a/v  1/v 0;
+     (a*R^2*Fz*K_mu)/(J*v)   -(Fz*K_mu*R^2)/(J*v)  0;
+     0     1/R   0];
+% B = nxu = 3x2 
+B = [R/J 0;
+     0 R/J;
+     0 0];
+% C = yxn = 3x3 
+C = [0     0    k_reifen;
+    Fz*K_mu*R  J  -k_reifen;
+    Fz*K_mu*R  J  0];
+
+% D = yxu = 3x2
+D = [0 0;
+     0 0;
+     0 0];
 
 %% Define Plant Model and MPC Controller
 % The linear plant model has two inputs and two outputs.
@@ -49,8 +69,9 @@ m = 2;   % control horizon
 mpcobj = mpc(plant,Ts,p,m);
 %%
 % Define constraints on the manipulated variable.
-mpcobj.MV = struct('Min',0,'Max',1530,'RateMin',-100,'RateMax',100);
-mpcobj.OV = struct('Min',0,'Max',2000);
+%mpcobj.MV = struct('Min',{-3;-2;-2},'Max',{3;2;2},'RateMin',{-1000;-1000;-1000});
+mpcobj.MV = struct('Min',{0,0},'Max',{225,1500},'RateMin',{-100,-100},'RateMax',{100,100});
+mpcobj.OV = struct('Min',{0;0;0},'Max',{225;1275;1500});
 
 % Define non-diagonal output weight. Note that it is specified inside a
 % cell array.
@@ -67,13 +88,13 @@ Tf = round(Tstop/Ts);     % number of simulation steps
 r = Td;
 %%
 %Run the closed-loop simulation and plot results.
-[y,t,u] = sim(mpcobj,Tf,r);
-subplot(211)
-plot(t,r-y);grid
-title('Ref(t)-yIst(t)');
-subplot(212)
-plot(t,u);grid
-title('u(t)');
+%[y,t,u] = sim(mpcobj,Tf,r);
+%subplot(211)
+%plot(t,r-y);grid
+%title('Ref(t)-yIst(t)');
+%subplot(212)
+%plot(t,u);grid
+%title('u(t)');
 
 %%
 % Now simulate closed-loop MPC in Simulink(R).
